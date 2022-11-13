822f871ed57a83fb3e372f748a078a1795ea7398
Kelvin Obidozie <kelvinobidozie@programmer.net>
2022-11-13 21:29:00 +0100

    A vanilla js admin dashboard

---
+++ orders.js
@@ -0,0 +1,63 @@
+const Orders = [
+
+    {productName: 'Foldable Mini Drone',
+    productNumber: '254687',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+    },
+    {productName: 'Foldable Mini Drone',
+    productNumber: '254687',
+    paymentStatus: 'Paid',
+    shipping: 'Declined'
+    },
+    {productName: 'Foldable Mini Drone',
+    productNumber: '894687',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+    },
+    {productName: 'Foldable Mini Drone',
+    productNumber: '254687',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+    },
+    {productName: 'Foldable Mini Drone',
+    productNumber: '36598',
+    paymentStatus: 'Paid',
+    shipping: 'Declined'
+    },
+    {productName: 'Foldable Mini Drone',
+    productNumber: '125478',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+    },
+    {productName: 'Foldable Mini Drone',
+    productNumber: '254687',
+    paymentStatus: 'Due',
+    shipping: 'Declined'
+    },
+    {productName: 'Foldable Mini Drone',
+    productNumber: '254687',
+    paymentStatus: 'Paid',
+    shipping: 'Pending'
+    },
+    {productName: 'Foldable Mini Drone',
+    productNumber: '12458',
+    paymentStatus: 'Due',
+    shipping: 'Declined'
+    },
+    {productName: 'Foldable Mini Drone',
+    productNumber: '254687',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+    },
+    {productName: 'Foldable Mini Drone',
+    productNumber: '254687',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+    },
+    {productName: 'Foldable Mini Drone',
+    productNumber: '254687',
+    paymentStatus: 'Paid',
+    shipping: 'Declined'
+    },
+]
